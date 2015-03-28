8c36b80b9fad2101b3768ae1e2ddb2c23b823d47
Wen Gu <guw@sunyit.edu>
2015-03-28 16:13:03 -0400

    Base setup for job details

--- introvita/introvita_user_interact/urls.py
+++ introvita/introvita_user_interact/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from introvita_user_interact import views
 
 urlpatterns = patterns('',
-                       url(r'employer/jobs/', views.employerJobs, name='index'),
+                       url(r'employer/jobs', views.employer_jobs, name='index'),
+                       url(r'jobs/jobdetail/(?P<job_id>\d+)', views.job_detail, name='job_detail'),
                        )
--- introvita/introvita_user_interact/views.py
+++ introvita/introvita_user_interact/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 
-def employerJobs(request):
-    return HttpResponse("Employer Jobs Response.")
+def employer_jobs(request):
+    template = loader.get_template('employer/jobs.html')
+    context = RequestContext(request, {
+        'response_message': 'Employer Jobs Response.',
+    })
+    return HttpResponse(template.render(context))
 
+
+def job_detail(request, job_id):
+    template = loader.get_template('jobs/jobdetail.html')
+    context = RequestContext(request, {
+        'response_message': 'Job Details Response. Job ID: ' + job_id,
+    })
+    return HttpResponse(template.render(context))

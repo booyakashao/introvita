8d68aa468912fb957dec8b6734faef224669031c
Wen Gu <guw@sunyit.edu>
2015-03-28 09:26:12 -0400

    Settings commit
    Please remember to install postgres and add to path
    
    PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"

--- introvita/introvita/settings.py
+++ introvita/introvita/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'introvita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'introvita',
+        'USER': 'introvita_user',
+        'PASSWORD': 'introvita_pwd_5321',
+        'HOST': 'smashthebeetles.com',
+        'PORT': '5432'
     }
 }
 
